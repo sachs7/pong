f847bb642877805c8fd5254acb043ef75e0a671c
Sachin Uplaonkar <up.sachi@gmail.com>
2015-10-12 22:26:05 -0700

    Update sum_of_pairs.py

--- Sum-of-Pairs/sum_of_pairs.py
+++ Sum-of-Pairs/sum_of_pairs.py
@@ -15,3 +15,9 @@ def sop(array_of_numbers, summed_value):
 print(sop([1, -2, 3, 0, -6, 1], -6))
 print(sop([10, 5, 2, 3, 7, 5], 10))
 print(sop([5, 9, 13, -3], 10))
+
+
+Results: 
+        [0, -6]
+        [3, 7]
+        [13, -3]
