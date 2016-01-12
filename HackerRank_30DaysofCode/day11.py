bfe91b10a9075b326517ec61acdcadf2e028cc07
Sachin Uplaonkar <up.sachi@gmail.com>
2016-01-12 10:16:38 -0800

    Create day11.py

---
+++ HackerRank_30DaysofCode/day11.py
@@ -0,0 +1,13 @@
+#!/bin/python3
+
+import sys
+
+arr = []
+for arr_i in range(6):
+    arr_t = [int(arr_temp) for arr_temp in input().strip().split(' ')]
+    arr.append(arr_t)
+fin = []
+for i in range(4):
+    for j in range(4):
+        fin.append(arr[i][j]+arr[i][j+1]+arr[i][j+2]+arr[i+1][j+1]+arr[i+2][j]+arr[i+2][j+1]+arr[i+2][j+2])
+print(max(fin))
