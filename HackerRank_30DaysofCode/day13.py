fd6258a32b815abc6f4c3ac5ea7084570748de96
Sachin Uplaonkar <up.sachi@gmail.com>
2016-01-13 06:47:24 -0800

    Create day13.py

---
+++ HackerRank_30DaysofCode/day13.py
@@ -0,0 +1,9 @@
+#Write MyBook class
+class MyBook(Book):
+    def __init__(self, title, author, price):
+        super().__init__(title, author)
+        self.price = price
+    def display(self):
+        print("Title:", title)
+        print("Author:", author)
+        print("Price:", price)
