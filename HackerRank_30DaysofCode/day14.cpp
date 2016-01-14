7c1bbfa60f2853428ba21abfdc92431b75bf488a
Sachin Uplaonkar <up.sachi@gmail.com>
2016-01-14 09:28:36 -0800

    Create day14.cpp

---
+++ HackerRank_30DaysofCode/day14.cpp
@@ -0,0 +1,52 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+class Difference {
+    private:
+    vector<int> elements;
+  
+  	public:
+  	int maximumDifference;
+  	
+  		// Add your code here
+    Difference(vector<int> e) {
+        elements = e;
+    }
+
+    int computeDifference() {
+        auto min_value = min_element(elements.begin(), elements.end());
+        auto max_value = max_element(elements.begin(), elements.end());
+
+        maximumDifference = (*max_value) - (*min_value);
+        
+        return maximumDifference;
+    }
+    
+    }; // End of Difference class
+
+int main() {
+    int N;
+    cin >> N;
+    
+    vector<int> a;
+    
+    for (int i = 0; i < N; i++) {
+        int e;
+        cin >> e;
+        
+        a.push_back(e);
+    }
+    
+    Difference d(a);
+    
+    d.computeDifference();
+    
+    cout << d.maximumDifference;
+    
+    return 0;
+}
