fabcfa2e8ae4469257ed335ff4306326a692da65
Sachin Uplaonkar <up.sachi@gmail.com>
2016-01-15 09:28:40 -0800

    Update day14.py

--- HackerRank_30DaysofCode/day14.py
+++ HackerRank_30DaysofCode/day14.py
@@ -2,3 +2,15 @@
         maxi = max(self.__elements)
         mini = min(self.__elements)
         self.maximumDifference = maxi - mini
+
+ Below is the alternate Python solution:
+
+	# Add your code here
+    def computeDifference(self):
+        lis = []
+        for i in range(len(self.__elements)):
+            for j in range(1, len(self.__elements)):
+                if i == j:
+                    continue
+                lis.append(abs(self.__elements[i] - self.__elements[j]))
+        self.maximumDifference = max(lis)
