00e78369a23a211558667b38c9385d02e5d696ab
Sachin Uplaonkar <up.sachi@gmail.com>
2017-01-22 19:29:17 -0800

    Update email_xkcd.py

--- XKCD/email_xkcd.py
+++ XKCD/email_xkcd.py
@@ -4,8 +4,8 @@ from email.mime.multipart import MIMEMultipart
 
 from download_xkcd import new_comic_number
 
-email_from = <EMAIL address from which you want to send>
-recipients = [list of comma seperated EMAIL addresses]
+email_from = <EMAIL ID>
+recipients = <LIST of EMAIL IDs>
 file_to_send = new_comic_number() + '.png'
 username = email_from
 password = <PASSWORD>
@@ -16,16 +16,18 @@ msg['bcc'] = ', '.join(recipients)
 msg['Subject'] = 'XKCD comic # ' + new_comic_number()
 
 ''' Create's a file COMIC_RECORDS.TXT to hold the previous Comic's number, so that same comic should not be emailed to
-recipients if there are no new comics '''
+recipients if there are no new comics.
+
+If using cron job, provide complete path to the text files in below function.'''
 
 
 def comic_ledger():
-    with open('comic_records.txt', 'r') as f:
+    with open('/give/complete/path/if/using/cron_job/comic_records.txt', 'r') as f:
         if new_comic_number() in f:
             print('SORRY! No new comics')
             return exit()
         else:
-            with open('comic_records.txt', 'w') as f:
+            with open('/give/complete/path/if/using/cron_job/comic_records.txt', 'w') as f:
                 f.write(new_comic_number())
 
 
